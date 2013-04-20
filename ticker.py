0f4b69a2311bd63684e988f67b244030bdc68b66
cg1609 <craig.glastonbury09@imperial.ac.uk>
2013-04-20 16:55:11 +0200

    scraper.py
    A website scraper that acts as a BTC price ticker for your desktop (Albeit in terminal) - pushes prices every 3 seconds.

---
+++ ticker.py
@@ -0,0 +1,28 @@
+import requests
+import re
+from sys import stdout
+from time import sleep
+
+def get_page(url):
+    r = requests.get(url)
+ #   print r.status_code
+    content = r.text.encode('utf-8', 'ignore')
+    return content
+
+if __name__ == "__main__":
+    try:
+        while True:
+            url = 'https://mtgox.com'
+            content = get_page(url)
+            content = content.replace('\n', '')
+
+
+            content_pattern = re.compile('li id="lastPrice".*?>.*?>(.*?)<')
+            result = re.findall(content_pattern, content)
+            data = result[0]
+            stdout.write('\rLast BTC price: ' +(data))
+            stdout.flush()
+            sleep(3)
+    except:
+        raise
+
